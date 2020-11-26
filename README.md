e66ae84a81a5746e06d97de4d6af9cf3eca94b73
Manu Phatak <bionikspoon@gmail.com>
2020-11-26 14:15:07 -0800

    Setup CI (#1)
    * refactor to with IO util
    
    * extract utils
    
    * Add circle ci config
    
    * touch CI
    
    * echo pwd
    
    * update working directory
    
    * use org_name and repo_name
    
    * use env template syntax
    
    * use parameters
    
    * use default build job
    
    * hardcode repo name
    
    * add docker hub auth
    
    * report coverage
    
    * add badges to readme
    
    * use codecov bash script to upload
    
    * remove orb

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![codecov](https://codecov.io/gh/bionikspoon/AdventOfCode2019-GoLang/branch/main/graph/badge.svg?token=N1JHDHYZX5)](https://codecov.io/gh/bionikspoon/AdventOfCode2019-GoLang)
+
+[![bionikspoon](https://circleci.com/gh/bionikspoon/AdventOfCode2019-GoLang.svg?style=svg)](https://circleci.com/gh/bionikspoon/AdventOfCode2019-GoLang)
+
 # AdventOfCode2019-GoLang
 A practice repo, getting ready for adventofcode.com/2020
--- src/Day01/Day01.go
+++ src/Day01/Day01.go
@@ -1,34 +1,26 @@
 package day01
 
-import (
-	"strconv"
-	"strings"
-)
-
 // Part1 solves for the fuel requirement for modules
 func Part1(input string) string {
-
-	moduleMasses := readInts(input)
-	fuelRequired := sumWith(fuelRequiredForMass, moduleMasses)
-
-	return strconv.Itoa(fuelRequired)
+	return withIO(input, sumWith(fuelRequiredForMass))
 }
 
 // Part2 solves for the fuel requirement for additional fuel
 func Part2(input string) string {
-	moduleMasses := readInts(input)
-	fuelRequired := sumWith(fuelRequiredForFuel, moduleMasses)
 
-	return strconv.Itoa(fuelRequired)
+	return withIO(input, sumWith(fuelRequiredForFuel))
 }
 
-func sumWith(fn func(int) int, masses []int) int {
-	sum := 0
-	for _, mass := range masses {
-		sum += fn(mass)
-	}
+func sumWith(fn func(int) int) func([]int) int {
 
-	return sum
+	return func(masses []int) int {
+		sum := 0
+		for _, mass := range masses {
+			sum += fn(mass)
+		}
+
+		return sum
+	}
 }
 
 func fuelRequiredForMass(mass int) int {
@@ -43,22 +35,4 @@ func fuelRequiredForFuel(mass int) int {
 	}
 
 	return 0
-}
-
-func readInts(input string) (ints []int) {
-	lines := strings.Split(strings.TrimSpace(input), "\n")
-
-	for _, line := range lines {
-		i, err := strconv.Atoi(line)
-		check(err)
-		ints = append(ints, i)
-	}
-
-	return
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
