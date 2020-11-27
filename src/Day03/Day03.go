58c983345788fbce0e93fc506028644347d6b053
Manu Phatak <manu.phatak@procore.com>
2020-11-27 13:32:42 -0800

    Solve part 1

--- src/Day03/Day03.go
+++ src/Day03/Day03.go
@@ -1,9 +1,192 @@
 package day03
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func Part1(input string) string {
-	return input
+	fields := strings.Fields(input)
+
+	distance, err := NewDiagram(fields)
+	if err != nil {
+		panic(err)
+	}
+	return strconv.Itoa(distance)
 }
 
 func Part2(input string) string {
 	return input
+}
+
+type Direction int
+
+const (
+	U Direction = iota
+	R
+	D
+	L
+)
+
+type Instruction struct {
+	direction Direction
+	distance  int
+}
+
+type Wire struct {
+	index        int
+	instructions []Instruction
+}
+
+type Coordinates struct {
+	x, y int
+}
+
+func NewDiagram(serializedWires []string) (int, error) {
+	wires := make([]*Wire, len(serializedWires))
+	for i, serializedWire := range serializedWires {
+		serializedInstructions := strings.Split(serializedWire, ",")
+
+		instructions := make([]Instruction, len(serializedInstructions))
+
+		for j, serializedInstruction := range serializedInstructions {
+			instruction, err := deserializeInstruction(serializedInstruction)
+
+			if err != nil {
+				return 0, err
+			}
+
+			instructions[j] = instruction
+		}
+
+		wires[i] = &Wire{i, instructions}
+	}
+
+	grid := make(map[Coordinates][]*Wire)
+
+	for _, wire := range wires {
+		position := Coordinates{0, 0}
+
+		for _, instruction := range wire.instructions {
+			for i := 0; i < instruction.distance; i++ {
+				switch instruction.direction {
+				case U:
+					position.y++
+				case R:
+					position.x++
+				case D:
+					position.y--
+				case L:
+					position.x--
+				}
+
+				_, ok := grid[position]
+
+				if !ok {
+					grid[position] = []*Wire{}
+				}
+
+				if positionContains(grid[position], wire) {
+					continue
+				}
+
+				grid[position] = append(grid[position], wire)
+			}
+		}
+	}
+
+	intersections := []Coordinates{}
+
+	for coordinates, wires := range grid {
+		if len(wires) == 1 {
+			continue
+		}
+
+		intersections = append(intersections, coordinates)
+	}
+
+	fmt.Println(intersections)
+
+	intersectionDistances := make([]int, len(intersections))
+	centralPort := Coordinates{0, 0}
+
+	for i, intersection := range intersections {
+		intersectionDistances[i] = manhattanDistance(intersection, centralPort)
+	}
+
+	fmt.Println(intersectionDistances)
+
+	return minInt(intersectionDistances), nil
+}
+
+func manhattanDistance(a, b Coordinates) int {
+	return intAbs(a.x-b.x) + intAbs(a.y-b.y)
+}
+
+func minInt(slice []int) int {
+	var min int
+	for index, element := range slice {
+		if index == 0 || element < min {
+			min = element
+		}
+	}
+	return min
+}
+
+func intAbs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func positionContains(gridPosition []*Wire, wire *Wire) bool {
+	for _, candidate := range gridPosition {
+		if candidate == wire {
+			return true
+		}
+	}
+
+	return false
+}
+
+func deserializeInstruction(serializedInstruction string) (Instruction, error) {
+	re, err := regexp.Compile(`([URDL])(\d+)`)
+	if err != nil {
+		return Instruction{}, err
+	}
+
+	groups := re.FindStringSubmatch(serializedInstruction)
+	direction, err := deserializeDirection(groups[1])
+	if err != nil {
+		return Instruction{}, err
+	}
+	distance, err := strconv.Atoi(groups[2])
+	if err != nil {
+		return Instruction{}, err
+
+	}
+
+	return Instruction{
+		direction: direction,
+		distance:  distance,
+	}, nil
+}
+
+func deserializeDirection(serializedDirection string) (Direction, error) {
+	switch serializedDirection {
+	case "U":
+		return U, nil
+	case "R":
+		return R, nil
+	case "D":
+		return D, nil
+	case "L":
+		return L, nil
+	default:
+		return 0, fmt.Errorf("Unknown direction %v", serializedDirection)
+
+	}
 }
--- src/Day03/Day03_test.go
+++ src/Day03/Day03_test.go
@@ -1,14 +1,12 @@
 package day03
 
 import (
-	"testing"
-
 	"bionikspoon/go-advent-of-code-2019/lib/testutils"
+	"testing"
 )
 
 func TestPart1(t *testing.T) {
-	t.Skip("skipping")
-	want := "hello santa"
+	want := "731"
 
 	if got := Part1(testutils.ReadFile(t, "input.txt")); got != want {
 		t.Errorf("Part1() = %v, want %v", got, want)
@@ -21,5 +19,33 @@ func TestPart2(t *testing.T) {
 
 	if got := Part2(testutils.ReadFile(t, "input.txt")); got != want {
 		t.Errorf("Part2() = %v, want %v", got, want)
+	}
+}
+
+func TestNewDiagram(t *testing.T) {
+	type args struct {
+		serializedWires []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{"case", args{[]string{"R8,U5,L5,D3", "U7,R6,D4,L4"}}, 6, false},
+		{"case", args{[]string{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"}}, 159, false},
+		{"case", args{[]string{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"}}, 135, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewDiagram(tt.args.serializedWires)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDiagram() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("NewDiagram() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
