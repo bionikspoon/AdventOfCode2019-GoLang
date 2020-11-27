316c9c49f6115f8ddf0fa2c372d1e337ef3cff0a
Manu Phatak <manu.phatak@procore.com>
2020-11-26 17:50:05 -0800

    solve part 1'

--- src/Day02/Day02.go
+++ src/Day02/Day02.go
@@ -1,10 +1,91 @@
 package day02
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Part1(input string) string {
-	return input
+	opcodes := deserialize(input)
+
+	opcodes[1] = 12
+	opcodes[2] = 2
+
+	nextOpcodes, err := runProgram(opcodes)
+	check(err)
+
+	return strconv.Itoa(nextOpcodes[0])
 }
 
 func Part2(input string) string {
 
 	return input
+}
+
+func run(input string) string {
+	opcodes := deserialize(input)
+
+	nextOpcodes, err := runProgram(opcodes)
+	check(err)
+
+	return serialize(nextOpcodes)
+}
+
+func runProgram(opcodes []int) ([]int, error) {
+	const maxIterations = 100
+
+	i := 0
+
+	for j := 0; j <= maxIterations; j++ {
+		opcode := opcodes[i]
+
+		switch opcode {
+		case 1:
+			opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] + opcodes[opcodes[i+2]]
+			i += 4
+
+		case 2:
+			opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] * opcodes[opcodes[i+2]]
+			i += 4
+
+		case 99:
+			return opcodes, nil
+
+		default:
+			return nil, fmt.Errorf("Unknown opcode %+v", opcode)
+		}
+	}
+
+	return nil, errors.New("Program never finished")
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func deserialize(input string) []int {
+	opcodes := []int{}
+
+	for _, s := range strings.Split(input, ",") {
+		opcode, err := strconv.Atoi(s)
+		check(err)
+
+		opcodes = append(opcodes, opcode)
+	}
+
+	return opcodes
+}
+
+func serialize(opcodes []int) string {
+	codes := make([]string, len(opcodes))
+
+	for index, opcode := range opcodes {
+		codes[index] = strconv.Itoa(opcode)
+	}
+
+	return strings.Join(codes, ",")
 }
--- src/Day02/Day02_test.go
+++ src/Day02/Day02_test.go
@@ -2,11 +2,12 @@ package day02
 
 import (
 	"io/ioutil"
+	"strings"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
-	want := "hello santa"
+	want := "5305097"
 
 	if got := Part1(readFile(t, "input.txt")); got != want {
 		t.Errorf("Part1() = %v, want %v", got, want)
@@ -14,6 +15,7 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
+	t.SkipNow()
 	want := "hello santa"
 
 	if got := Part2(readFile(t, "input.txt")); got != want {
@@ -26,5 +28,28 @@ func readFile(t *testing.T, filename string) string {
 	if err != nil {
 		t.Fatal(err)
 	}
-	return string(input)
+	return strings.TrimSpace(string(input))
+}
+
+func Test_run(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"addition", args{"1,0,0,0,99"}, "2,0,0,0,99"},
+		{"multiplication", args{"2,3,0,3,99"}, "2,3,0,6,99"},
+		{"edge case", args{"2,4,4,5,99,0"}, "2,4,4,5,99,9801"},
+		{"chaining opcodes", args{"1,1,1,4,99,5,6,0,99"}, "30,1,1,4,2,5,6,0,99"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := run(tt.args.input); got != tt.want {
+				t.Errorf("runProgram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
