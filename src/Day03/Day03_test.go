64cedd3c3b57a42ee54bab021a4d40ce1eb95164
Manu Phatak <manu.phatak@procore.com>
2020-11-26 21:55:18 -0800

    Setup Day 03

---
+++ src/Day03/Day03_test.go
@@ -0,0 +1,23 @@
+package day03
+
+import (
+	"testing"
+
+	"lib/testutils"
+)
+
+func TestPart1(t *testing.T) {
+	want := "hello santa"
+
+	if got := Part1(testutils.ReadFile(t, "input.txt")); got != want {
+		t.Errorf("Part1() = %v, want %v", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	want := "hello santa"
+
+	if got := Part2(testutils.ReadFile(t, "input.txt")); got != want {
+		t.Errorf("Part2() = %v, want %v", got, want)
+	}
+}
